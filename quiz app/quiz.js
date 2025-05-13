52ccf4bd8cdeeaa879d494d48ade7586910c4266
Pratik Humagain <pratikhumagain9808@gmail.com>
2025-05-13 16:34:53 +0545

    quiz

---
+++ quiz app/quiz.js
@@ -0,0 +1,204 @@
+var quiz = {
+  "JS": [
+    {
+      "id": 1,
+      "question": "What is the capital of the United States?",
+      "options": [{ "a": "New York", "b": "Los Angeles", "c": "Washington, D.C.", "d": "Chicago" }],
+      "answer": "Washington, D.C.",
+      "score": 0,
+      "status": ""
+    },
+    {
+      "id": 2,
+      "question": "Which U.S. state is known as the 'Sunshine State'?",
+      "options": [{ "a": "California", "b": "Florida", "c": "Nevada", "d": "Texas" }],
+      "answer": "Florida",
+      "score": 0,
+      "status": ""
+    },
+    {
+      "id": 3,
+      "question": "Which ocean borders the east coast of the United States?",
+      "options": [{ "a": "Indian Ocean", "b": "Pacific Ocean", "c": "Arctic Ocean", "d": "Atlantic Ocean" }],
+      "answer": "Atlantic Ocean",
+      "score": 0,
+      "status": ""
+    },
+    {
+      "id": 4,
+      "question": "What year did the U.S. declare independence?",
+      "options": [{ "a": "1776", "b": "1789", "c": "1804", "d": "1750" }],
+      "answer": "1776",
+      "score": 0,
+      "status": ""
+    },
+    {
+      "id": 5,
+      "question": "Who was the first President of the United States?",
+      "options": [{ "a": "John Adams", "b": "Thomas Jefferson", "c": "George Washington", "d": "James Madison" }],
+      "answer": "George Washington",
+      "score": 0,
+      "status": ""
+    },
+    {
+      "id": 6,
+      "question": "Which U.S. state is the largest by area?",
+      "options": [{ "a": "Texas", "b": "California", "c": "Alaska", "d": "Montana" }],
+      "answer": "Alaska",
+      "score": 0,
+      "status": ""
+    },
+    {
+      "id": 7,
+      "question": "Which U.S. state has the most people?",
+      "options": [{ "a": "New York", "b": "Texas", "c": "Florida", "d": "California" }],
+      "answer": "California",
+      "score": 0,
+      "status": ""
+    },
+    {
+      "id": 8,
+      "question": "Which river is the longest in the United States?",
+      "options": [{ "a": "Mississippi", "b": "Colorado", "c": "Missouri", "d": "Ohio" }],
+      "answer": "Missouri",
+      "score": 0,
+      "status": ""
+    },
+    {
+      "id": 9,
+      "question": "What is the national bird of the United States?",
+      "options": [{ "a": "Golden Eagle", "b": "Bald Eagle", "c": "Red-tailed Hawk", "d": "Osprey" }],
+      "answer": "Bald Eagle",
+      "score": 0,
+      "status": ""
+    },
+    {
+      "id": 10,
+      "question": "Which state was the last to join the Union?",
+      "options": [{ "a": "Alaska", "b": "Hawaii", "c": "Arizona", "d": "New Mexico" }],
+      "answer": "Hawaii",
+      "score": 0,
+      "status": ""
+    },
+    {
+      "id": 11,
+      "question": "Who wrote the Declaration of Independence?",
+      "options": [{ "a": "George Washington", "b": "Thomas Jefferson", "c": "Benjamin Franklin", "d": "John Adams" }],
+      "answer": "Thomas Jefferson",
+      "score": 0,
+      "status": ""
+    },
+    {
+      "id": 12,
+      "question": "What is the national anthem of the United States?",
+      "options": [{ "a": "America the Beautiful", "b": "God Bless America", "c": "The Star-Spangled Banner", "d": "My Country, 'Tis of Thee" }],
+      "answer": "The Star-Spangled Banner",
+      "score": 0,
+      "status": ""
+    },
+    {
+      "id": 13,
+      "question": "How many stars are on the U.S. flag?",
+      "options": [{ "a": "48", "b": "49", "c": "50", "d": "51" }],
+      "answer": "50",
+      "score": 0,
+      "status": ""
+    },
+    {
+      "id": 14,
+      "question": "How many stripes are on the U.S. flag?",
+      "options": [{ "a": "13", "b": "14", "c": "15", "d": "12" }],
+      "answer": "13",
+      "score": 0,
+      "status": ""
+    },
+    {
+      "id": 15,
+      "question": "Which war ended with the signing of the Treaty of Paris in 1783?",
+      "options": [{ "a": "War of 1812", "b": "Revolutionary War", "c": "Civil War", "d": "Mexican-American War" }],
+      "answer": "Revolutionary War",
+      "score": 0,
+      "status": ""
+    },
+    {
+      "id": 16,
+      "question": "What is the smallest U.S. state by area?",
+      "options": [{ "a": "Connecticut", "b": "Delaware", "c": "Rhode Island", "d": "Vermont" }],
+      "answer": "Rhode Island",
+      "score": 0,
+      "status": ""
+    },
+    {
+      "id": 17,
+      "question": "Which city is known as the 'Windy City'?",
+      "options": [{ "a": "New York", "b": "Chicago", "c": "Boston", "d": "Detroit" }],
+      "answer": "Chicago",
+      "score": 0,
+      "status": ""
+    },
+    {
+      "id": 18,
+      "question": "What U.S. holiday is celebrated on the fourth Thursday of November?",
+      "options": [{ "a": "Veterans Day", "b": "Memorial Day", "c": "Thanksgiving", "d": "Labor Day" }],
+      "answer": "Thanksgiving",
+      "score": 0,
+      "status": ""
+    },
+    {
+      "id": 19,
+      "question": "Which U.S. state is famous for its potatoes?",
+      "options": [{ "a": "Iowa", "b": "Maine", "c": "Idaho", "d": "Nebraska" }],
+      "answer": "Idaho",
+      "score": 0,
+      "status": ""
+    },
+    {
+      "id": 20,
+      "question": "What is the name of the U.S. central banking system?",
+      "options": [{ "a": "The Treasury", "b": "The Fed", "c": "Bank of America", "d": "Chase National" }],
+      "answer": "The Fed",
+      "score": 0,
+      "status": ""
+    },
+    {
+      "id": 21,
+      "question": "Which U.S. president abolished slavery?",
+      "options": [{ "a": "George Washington", "b": "Thomas Jefferson", "c": "Abraham Lincoln", "d": "Andrew Jackson" }],
+      "answer": "Abraham Lincoln",
+      "score": 0,
+      "status": ""
+    },
+    {
+      "id": 22,
+      "question": "What is the highest court in the United States?",
+      "options": [{ "a": "The Senate", "b": "The Supreme Court", "c": "Court of Appeals", "d": "Federal District Court" }],
+      "answer": "The Supreme Court",
+      "score": 0,
+      "status": ""
+    },
+    {
+      "id": 23,
+      "question": "Which monument is located in South Dakota?",
+      "options": [{ "a": "Statue of Liberty", "b": "Mount Rushmore", "c": "Lincoln Memorial", "d": "Washington Monument" }],
+      "answer": "Mount Rushmore",
+      "score": 0,
+      "status": ""
+    },
+    {
+      "id": 24,
+      "question": "Which branch of government makes federal laws?",
+      "options": [{ "a": "Executive", "b": "Legislative", "c": "Judicial", "d": "Cabinet" }],
+      "answer": "Legislative",
+      "score": 0,
+      "status": ""
+    },
+    {
+      "id": 25,
+      "question": "What is the official currency of the United States?",
+      "options": [{ "a": "Euro", "b": "Pound", "c": "Dollar", "d": "Yen" }],
+      "answer": "Dollar",
+      "score": 0,
+      "status": ""
+    }
+  ]
+}
